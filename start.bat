cc823cd4050b318610ddc66566bb30cab3a6c126
sakitoru1x1 <rlimaenko@gmail.com>
2026-04-18 12:17:55 +0000

    Revert "feat: start.bat показывает список новых коммитов после git pull"
    This reverts commit da0218c18a53321637a4a26e7fac16159641fec4.

--- start.bat
+++ start.bat
@@ -1,23 +1,4 @@
 @echo off
-chcp 65001 >nul
-echo ======================================
-echo   Avito News Parser
-echo ======================================
-echo.
-echo [1/2] Проверка обновлений...
-for /f "delims=" %%i in ('git rev-parse HEAD') do set OLD=%%i
-git pull --quiet
-for /f "delims=" %%i in ('git rev-parse HEAD') do set NEW=%%i
-if "%OLD%"=="%NEW%" (
-    echo   Всё актуально.
-) else (
-    echo.
-    echo   Новые изменения:
-    git log %OLD%..%NEW% --no-merges --pretty=format:"    - %%s"
-    echo.
-)
-echo.
-echo [2/2] Запуск программы...
-echo.
+git pull
 python main.py
 pause
